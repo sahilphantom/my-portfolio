47f1b4d8d5278ac074694f238d1235dceefb7deb
sahilphantom <sahilmahmood188@gmail.com>
2025-01-20 00:51:18 +0500

    updated code

--- src/components/canvas/BallCanvas.jsx
+++ src/components/canvas/BallCanvas.jsx
@@ -1,13 +1,4 @@
-// import React, { Suspense } from 'react'
-// import { Canvas } from "@react-three/fiber";
-// import {
-//     OrbitControls,
-//     Preload,
-//   } from "@react-three/drei";
-// import CanvasLoader from '../Loader';
-// import Ball from './Ball';
 
-// const BallCanvas = ({ icon }) => {
 //     return (
 //       <Canvas
 //         frameloop='demand'
