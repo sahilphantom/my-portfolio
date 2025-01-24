0b95e3cce2b5713ae82b7f85e160bce295147e0d
sahilphantom <sahilmahmood188@gmail.com>
2025-01-24 17:30:02 +0500

    updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,44 +1,21 @@
-// import React from "react";
-// import { BallCanvas } from "./canvas";
-// import { SectionWrapper } from "../hoc";
-// import { technologies } from "../constants";
 
-// const Tech = () => {
-//   return (
-//     <div className='flex flex-row flex-wrap justify-center gap-10 '>
-//       {technologies.map((technology) => (
-//         <div className='w-28 h-28' key={technology.name}>
-//           <BallCanvas icon={technology.icon} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default SectionWrapper(Tech, "");
 
 import React from "react";
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-import { useMediaQuery } from "../hooks/useMediaQuery";
 
 const Tech = () => {
-  const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <div className='flex flex-row flex-wrap justify-center gap-6 sm:gap-10'>
       {technologies.map((technology) => (
         <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
-          {isMobile ? (
             <img 
               src={technology.icon || "/placeholder.svg"} 
               alt={technology.name} 
               className="w-full h-full object-contain"
             />
-          ) : (
-            <BallCanvas icon={technology.icon} />
-          )}
+        
         </div>
       ))}
     </div>
